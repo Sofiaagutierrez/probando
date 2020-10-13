41005b9bc68db4c3eaf54948a481b4b9170a7e07
sofiaagutierrez <slgutierrez@uc.cl>
2020-10-13 20:53:00 -0300

    primer mensaje de la clase

--- Github.R
+++ Github.R
@@ -1,6 +1,15 @@
+
+
 install.packages("tidyverse")
 library(tidyverse)
 install.packages("kableExtra")
 library(kableExtra)
 install.packages("knitr")
 library(knitr)
+
+#git config --global -l
+user.name=sofiaagutierrez
+user.email=slgutierrez@uc.cl
+http.proxy=proxy.Sofiaagutierrez/probando
+
+
